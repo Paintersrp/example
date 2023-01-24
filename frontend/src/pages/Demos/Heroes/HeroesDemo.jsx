60d2a415074808769beae713158e428a26a97ae4
Ryan Painter <paintersrp@gmail.com>
2023-01-23 21:47:40 -0500

    ^

--- frontend/src/pages/Demos/Heroes/HeroesDemo.jsx
+++ frontend/src/pages/Demos/Heroes/HeroesDemo.jsx
@@ -6,19 +6,40 @@ import Demo from "../Demo";
 
 const items = [
   {
+    index: 0,
     image: "images/masonry/img1.jpg",
     buttonText: "View Project",
     buttonLink: "/item-1",
   },
   {
+    index: 1,
     image: "images/masonry/img2.jpg",
     buttonText: "View Project",
     buttonLink: "/item-2",
   },
   {
+    index: 2,
     image: "images/masonry/img3.jpg",
     buttonText: "View Project",
     buttonLink: "/item-3",
+  },
+  {
+    index: 3,
+    image: "images/masonry/img4.jpeg",
+    buttonText: "View Project",
+    buttonLink: "/item-4",
+  },
+  {
+    index: 4,
+    image: "images/masonry/img5.jpeg",
+    buttonText: "View Project",
+    buttonLink: "/item-5",
+  },
+  {
+    index: 5,
+    image: "images/masonry/img6.jpeg",
+    buttonText: "View Project",
+    buttonLink: "/item-6",
   },
 ];
 
